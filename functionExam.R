2fef3a67edb32881af3c40c2307d3935e2134475
Peter <pmarincola@gmail.com>
2022-05-15 22:35:23 +0200

    Exercise2
    2.1 , 2.2

--- functionExam.R
+++ functionExam.R
@@ -9,19 +9,55 @@ payoff<-function(ST,K,Call=TRUE) {
   } else{
     res<-pmax(K-ST,0)
   }
-  plot(x=ST,y=res,type="l")
+  plot(x=ST,
+       y=res,
+       ylab="Payoff",
+       xlab="sottostante",
+       type="l")
   return(res)
 }
 
 # Funzione ex 1.2
-payoffButt<-function(ST,K,K1,K2,Call=TRUE) {
+payoffButt<-function(ST,K1,K2,J,J_plus_H,H,K_lin_conv,Call=TRUE) {
   if(Call){
-    res<-pmax(ST-K,0) - pmax(ST-K1,0) + pmax(ST-K2,0)
+    res<- J*pmax(ST-K1,0) - J_plus_H*pmax(ST- K_lin_conv,0) + H*pmax(ST-K2,0)
   } else{
     res<-pmax(K-ST,0)
   }
-  plot(x=ST,y=res,type="l")
+  plot(x=ST,
+       y=res,
+       main="Butterfly Strategy",
+       ylab="Payoff",
+       xlab="sottostante",
+       type="l")
   return(res)
+}
+
+
+# Funzione ex. 2.1
+EU_put <- function(S, K, t0, T, i, u, d){
+  r_free_bond <- (1+i)^(T-t0)
+  # At time t0, the price of the the financial derivative is equal to the market value of the replicating portfolio
+  qd = (r_free_bond-d)/(u-d)
+  qu = 1-qd
+  # Risky asset that has the following market price dynamics
+  ## payoff u
+  pu = pmax((K-S)*u,0)
+  ## payoff d
+  pd = pmax((K-S)*d,0)
+  # pricing formula
+  phi_t0 = (1/r_free_bond)*((qu*pu) + (qd*pd))
+  # Replicating portfolio
+  y = (1/S)*((pu-pd)/u-d)
+  return(list("price_t0" = phi_t0, "q" = c(qu, qd), "y" = y))
+}
+
+# Funzione ex. 2.2
+# put-call parity
+priceParity <- function(S,K,t0, T, i,u,d){
+  result <- EU_put(S, K, t0, T, i, u, d)
+  r_free_bond <- (1+i)^(T-t0)
+  return <- max(result$price_t0 + S - K*r_free_bond, 0)
 }
 
 
--- mainExam.R
+++ mainExam.R
@@ -2,29 +2,76 @@
 #Master DSE 2022
 #Marincola - Chiuch - Panelli ......
 
-#EX.1
+########### EX.1 ################################################################
 #Scrivete una funzione in R che calcola il payoff di una Call con posizione lunga.
 #Verificate il comportamento della funzione con un esempio numerico.
 
-#OK
+# Call for Long position
 source("functionExam.R")
+# Underlying
 S<-seq(1,3,length.out= 101)
 K <- 1.5
-debug("payoff")
+# Function
 aa<-payoff(ST=S,K=K)
 
 #Usando la funzione nel punto 1 disegnate il payoff di una strategia butterfly.
 
-#DA RIVEDERE
+#BUTTERFLY STRATEGY
 source("functionExam.R")
+# Underlying
 S<-seq(1,3,length.out= 101)
-#K<-1.5
-K <- 1.5
-K1 <- 2
-K2 <- 3
-debug("payoffButt")
-aa<-payoffButt(ST=S,K=K,K1=K1,K2=K2) 
+# J Long positions in a Call option with a Strike Price K1
+J <-1
+K1 <- 1.5
+# H Long positions in a Call option with a Strike Price K2
+H <-1
+K2 <- 2.5
+# J + H Short positions in a Call option with a Strike Price K (Linear Convex combination)
+J_plus_H <- J + H
+## omega
+w_omega <- J/(J+H)
+## Strike price K Linear Convex
+K_lin_conv <- w_omega*K1 + (1-w_omega)*K2
 
-#EX.2
-#Scrivete una funzione in R che valuta un'opzione europea di tipo put in un albero binomiale uniperiodale.
+aa<-payoffButt(ST=S,K1=K1,K2=K2,J=J,J_plus_H=J_plus_H,H=H,K_lin_conv=K_lin_conv) 
+
+
+########### EX.2 ##################################################################
+#2.1 Scrivete una funzione in R che valuta un'opzione europea di tipo put in un albero binomiale uniperiodale.
+
+# European Put Option
+# The Holder has the right to sell at maturity T the Underlying Asset S at the Strike Price K.
+# We have two dates: t0 ”‘Today”’ and T ”‘Maturity”’
+source("functionExam.R")
+# Underlying
+S = 50
+#Strike 
+K = 70
+#today
+t0 = 0
+#Maturity
+T = 1 
+i = 0.1
+u = 1.5
+d = 0.5
+
+result <- EU_put(S, K, t0, T, i, u, d)
+# Price of the financial derivate at t0
+sprintf("Price: %f",result$price_t0)
+# Replicating portfolio
+sprintf("y: %f",result$y)
+sprintf("q: %f",result$q)
+
+
+# 2.2 Scrivete una funzione in R che internamente richiama la funzione del punto 1 
+# e calcola il prezzo al tempo t0 di una call europea con la put-call parity 
+#(ricordatevi di utilizzare il regime composto anche nella put-call parity per 
+#  coerenza con il punto 1).
+source("functionExam.R")
+# Prezzo al tempo t_0
+callPrice_t0 <- priceParity(S, K, t0, T, i, u, d)
+
+# Verificate il comportamento delle due funzioni con un esempio numerico. 
+# Verificate che la call del punto 2 soddisfi i vincoli di Merton (regime composto).
+
 
